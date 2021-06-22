e65a5c2c2b00e299e1b1bd9f8007cb74d8ab5773
Dave Webb <dave-riroriro@dcw.co.nz>
2021-06-22 21:05:28 +1200

    Config reader tests now pass

--- config_reader_test.go
+++ config_reader_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
+	"git-notes/internal/types"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestJsonConfigReader_Read(t *testing.T) {
@@ -10,5 +12,5 @@ func TestJsonConfigReader_Read(t *testing.T) {
 	config, err := reader.Read("./git-notes.json.example")
 	assert.NoError(t, err)
 
-	assert.Equal(t, []string{"/Users/tanin/projects/personal-notes", "/Users/tanin/projects/another-personal-notes"}, config.Repos)
+	assert.Equal(t, []types.Repo{types.Repo{"/Users/ash/todos", "trunk"}, types.Repo{"/Users/ash/projects/personal-notes", "master"}}, config.Repos)
 }
